b19a86ca054dded22a1af06e9983ca582a41ef9f
LewyMistrz <42480581+LewyMistrz@users.noreply.github.com>
2018-10-15 08:44:56 +0200

    Update calulateStats.php

--- source/calulateStats.php
+++ source/calulateStats.php
@@ -6,5 +6,15 @@ require "connectToDatabase.php";
 	$dmg = mysqli_fetch_array($dmgMYSQL);
 	$dmg[0] = $dmg[0] * rand(75, 125) /100; // mnożone razy critchance 
 
+	$LVLquer="select lvl from player where nickname='$login'";
+	$lvlMYSQL= mysqli_query($i, $LVLquer);
+	$lvl = mysqli_fetch_array($lvlMYSQL);
+	
+	$STAMINAquer="select playerStamina from player where nickname='$login'";
+	$staminaMYSQL= mysqli_query($i, $STAMINAquer);
+	$stamina = mysqli_fetch_array($staminaMYSQL);
+	$stamina[0] = $stamina[0] * $lvl[0] *5; // stamina mnożona przez level a nastepnie mnożona na pięć
+
+
 
 ?> 
