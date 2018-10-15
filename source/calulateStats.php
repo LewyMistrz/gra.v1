d97be56ab206b3bfb15114f8a1724979b46f20f2
LewyMistrz <42480581+LewyMistrz@users.noreply.github.com>
2018-10-15 08:42:51 +0200

    Update calulateStats.php

--- source/calulateStats.php
+++ source/calulateStats.php
@@ -1,10 +1,10 @@
 <?php 
 require "connectToDatabase.php";
 
-$quer="select dmgStat from player where nickname='$login'";
-	$dmgMYSQL= mysqli_query($i, $quer);
+	$DMGquer="select dmgStat from player where nickname='$login'";
+	$dmgMYSQL= mysqli_query($i, $DMGquer);
 	$dmg = mysqli_fetch_array($dmgMYSQL);
-	$dmg[0] = $dmg[0] * rand(75, 125) /100;
+	$dmg[0] = $dmg[0] * rand(75, 125) /100; // mnożone razy critchance 
 
 
 ?> 
